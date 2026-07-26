3db8a92f5ee5d4294254a6ee687cc2ebb4ba84b4
Chotteee <36397833+Chotteee@users.noreply.github.com>
2026-07-27 01:34:11 +0800

    Update Nav.js

--- kwentopod/components/Nav.js
+++ kwentopod/components/Nav.js
@@ -6,10 +6,10 @@ export default function Nav() {
     <nav className="nav">
       <Link href="#" className="navLogo">Kwento<span>Pod</span></Link>
       <ul className="navLinks">
-        <li><a href="#services">Services</a></li>
-        <li><a href="#platforms">Platforms</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#pricing">Pricing</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#platforms">Platforms</a></li>
+        <li><a href="#services">Services</a></li>
         <li><a href="#faq">FAQ</a></li>
       </ul>
       <a href="#contact" className="navCta">Get Started</a>
